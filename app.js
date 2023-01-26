b61e88f5af85a346e2a2841a7e15ea2485b85362
Rayyan-Uddin <rayyanuddin007@gmail.com>
2023-01-26 22:48:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// Question # 1
+// alert("Welcome to out Webiste");
+
+// Question # 2
+// alert("Error! Please enter a valid Password");
+
+// Questino #3
+// alert("Welcom to JS Land...\n Happy Coding")
+
+// Question #4
+// alert("Welcome to JS Land")
+// alert("Happy Coding")
+
+// Question #5
+// Done
+
+// Question #6
+// Done
+
+// Question #7
+// Showing alert in a,b,c and d.
+
+
